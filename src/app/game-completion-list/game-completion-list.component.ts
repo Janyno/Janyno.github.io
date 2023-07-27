84983bc660ca1d3a91b6707f2723cdbac7100bd5
Janyno <janinolp@gmail.com>
2023-07-27 17:08:31 +0200

    updated list

--- src/app/game-completion-list/game-completion-list.component.ts
+++ src/app/game-completion-list/game-completion-list.component.ts
@@ -8,12 +8,34 @@ import { Game } from '../model/game';
 })
 export class GameCompletionListComponent {
   toPlay: Game[] = [
-    {name: 'TRADER LIFE SIMULATOR'},
-    {name: 'Chernobylite'}
+    {name: 'TRADER LIFE SIMULATOR', status: 'not_began'},
+    {name: 'Outward', status: 'not_began'},
+    {name: 'Tales of Arise', status: 'not_began'},
+    {name: 'Dyson Sphere Program', status: 'not_began'},
+    {name: 'Beyond: Two Souls', status: 'not_began'},
+    {name: 'Chernobylite', status: 'not_began'},
+    {name: 'God of War', status: 'not_began'},
+    {name: 'GTA 3', status: 'not_began'},
+    {name: 'GTA SA', status: 'not_began'},
+    {name: 'GTA VC', status: 'not_began'},
+    {name: 'Life is Strange 2', status: 'not_began'},
+    {name: 'Mad Max', status: 'not_began'},
+    {name: 'NieR: Automata', status: 'not_began'},
+    {name: 'Quantum Break', status: 'not_began'},
+    {name: 'Spyro Trilogy', status: 'not_began'},
+    {name: 'Above Snakes', status: 'not_began'},
+    {name: 'Yakuza 7', status: 'not_began'}
   ]
 
   toComplete: Game [] = [
-    {name: 'Red Dead Redemption 2', progress: 76, link: 'https://www.ign.com/maps/red-dead-redemption-2/world'},
-    {name: 'Yakuza 0', progress: 70}
+    {name: 'Red Dead Redemption 2', progress: 70, link: 'https://www.ign.com/maps/red-dead-redemption-2/world'},
+    {name: 'Yakuza 0', progress: 60},
+    {name: 'Dying Light 2', progress: 40}
   ]
+
+  statusColorMap: { [status: string]: string } = {
+    not_began: 'red',
+    in_progress: 'yellow',
+    completed: 'green'
+  };
 }
--- src/app/model/game.ts
+++ src/app/model/game.ts
@@ -1,5 +1,6 @@
 export interface Game {
     name: string,
+    status?: 'not_began' | 'in_progress' | 'completed',
     progress?: number,
     notes?: string,
     link?: string
