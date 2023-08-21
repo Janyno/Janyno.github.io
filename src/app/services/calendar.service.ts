0cd2b6cd9e3c0c6d219cb1c3c1cda003d7755960
Janyno <janinolp@gmail.com>
2023-08-21 22:07:56 +0200

    updated list

--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -21,8 +21,15 @@ export class CalendarService {
         {title: 'Starfield', start: new Date('2023-9-01'), color: { ...this.colors.blue }},
         {title: 'Payday 3', start: new Date('2023-9-18'), color: { ...this.colors.blue }},
         {title: 'Forza Motorsport', start: new Date('2023-10-9'), color: { ...this.colors.blue }},
-        {title: 'Cities Skylines 2', start: new Date('2023-10-24'), color: { ...this.colors.blue }}
-    ]
+        {title: 'Cities Skylines 2', start: new Date('2023-10-24'), color: { ...this.colors.blue }},
+        {title: 'Assassins Creed Mirage', start: new Date('2023-10-5'), color: { ...this.colors.blue }},
+        {title: 'The Crew Motorfest', start: new Date('2023-9-14'), color: { ...this.colors.blue }},
+        {title: 'The Lords of the Fallen', start: new Date('2023-10-13'), color: { ...this.colors.blue }},
+        {title: 'Alan Wake 2', start: new Date('2023-10-17'), color: { ...this.colors.blue }},
+        {title: 'Like a Dragon Gaiden', start: new Date('2023-11-9'), color: { ...this.colors.blue }},
+        {title: 'Call of Duty: Modern Warfare 3', start: new Date('2023-11-10'), color: { ...this.colors.blue }},
+        {title: 'Avatar: Frontiers of Pandora', start: new Date('2023-12-7'), color: { ...this.colors.blue }},
+      ]
 
     events: EventEntry[] = [
         {title: 'Gamescom Opening Night', start: new Date('2023-8-22'), startTime: '19:30', color: { ...this.colors.yellow }}
