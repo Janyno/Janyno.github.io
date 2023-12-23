d9c810b9018ed7a0e7396c56665464b57e96ffe4
Janyno <janinolp@gmail.com>
2023-12-23 15:55:14 +0100

    add snow effect yeah boy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  snowflakes: any[] = [];
+
+  ngOnInit(): void {
+    this.createSnowflakes();
+  }
+
+  createSnowflakes(): void {
+    for (let i = 0; i < 50; i++) {
+      this.snowflakes.push({
+        left: Math.random() * window.innerWidth,
+        speed: Math.random() * 2 + 0.5
+      });
+    }
+  }
+}
