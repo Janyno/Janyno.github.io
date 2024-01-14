5f59420eb667b6f34c57892b3c8f0fe7944931ab
Janyno <janinolp@gmail.com>
2024-01-14 17:08:26 +0100

    update

--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -168,15 +168,7 @@ export class CalendarService {
       {
         start: this.transformDateForSchedule([14, 1, 2024], [11, 0]), title: 'Sims 4', 
         end: this.transformDateForSchedule([14, 1, 2024], [23, 0]), color: this.getColor('greenyellow')
-      },
-      {
-        start: this.transformDateForSchedule([15, 1, 2024], [16, 0]), title: 'Eastern Exorcist', 
-        end: this.transformDateForSchedule([15, 1, 2024], [23, 0]), color: this.getColor('mistyrose')
-      },
-      {
-        start: this.transformDateForSchedule([16, 1, 2024], [16, 0]), title: 'Eastern Exorcist', 
-        end: this.transformDateForSchedule([16, 1, 2024], [23, 0]), color: this.getColor('mistyrose')
-      },
+      }
     ];
 
     getCalendarList() {
