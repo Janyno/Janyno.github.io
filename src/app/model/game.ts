fdead4ba3a2f156f4b0df0eb15f0144e48692d5e
Janyno <janinolp@gmail.com>
2024-02-11 16:08:32 +0100

    small update

--- src/app/model/game.ts
+++ src/app/model/game.ts
@@ -3,5 +3,6 @@ export interface Game {
     status: 'not_began' | 'in_progress' | 'completed' | 'wanna_play',
     playtime : number,
     vr?: boolean,
-    all_achievements?: boolean
+    all_achievements?: boolean,
+    includes_dlc?: boolean
 }
